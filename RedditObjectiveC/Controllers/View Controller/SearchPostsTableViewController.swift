347e532a056152976e236f616a4f7b3abc8d709b
Bryce Bradshaw <brycewbradshaw@icloud.com>
2020-05-06 12:34:09 -0600

    Storyboard completed

---
+++ RedditObjectiveC/Controllers/View Controller/SearchPostsTableViewController.swift
@@ -0,0 +1,56 @@
+//
+//  SearchPostsTableViewController.swift
+//  RedditObjectiveC
+//
+//  Created by Bryce Bradshaw on 5/6/20.
+//  Copyright © 2020 Bryce Bradshaw. All rights reserved.
+//
+
+import UIKit
+
+class SearchPostsTableViewController: UITableViewController {
+
+    // MARK: - Outlets
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    // MARK: - Properties
+    var posts: [WBBPost] = [] {
+        didSet{
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+    // MARK: - Lifecycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        searchBar.delegate = self
+        
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return posts.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "postCell", for: indexPath) as?
+            PostTableViewCell else { return UITableViewCell() }
+
+        return cell
+    }
+} // End Of Class
+
+extension SearchPostsTableViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard let searchTerm = searchBar.text, !searchTerm.isEmpty else { return }
+        
+        WBBPostController.sharedInstance().searchForPost(withSearchTerm: searchTerm) { (posts, error) in
+            self.posts = posts
+        }
+    }
+}
---
+++ RedditObjectiveC/Views/PostTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  PostTableViewCell.swift
+//  RedditObjectiveC
+//
+//  Created by Bryce Bradshaw on 5/6/20.
+//  Copyright © 2020 Bryce Bradshaw. All rights reserved.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    // MARK: - Outlets
+    @IBOutlet weak var postTitleLabel: UILabel!
+    @IBOutlet weak var postUpsLabel: UILabel!
+    @IBOutlet weak var postCommentsLabel: UILabel!
+    
+
+}
